5cf667a2cf21b719784f5bc236e452554da5bcf7
yoshihikosuzuki <ys.neoteny@gmail.com>
2020-08-26 18:01:13 +0900

    add global count distribution plot

--- dash_kmer_profile.py
+++ dash_kmer_profile.py
@@ -1,4 +1,6 @@
 import os
+import argparse
+from collections import Counter
 from typing import List
 import plotly_light as pl
 import plotly.graph_objects as go
@@ -8,48 +10,65 @@ from dash.dependencies import Input, Output, State
 import dash
 import plotly.io as pio
 from BITS.util.proc import run_command
-pio.templates.default = 'plotly_white'
 
+
+pio.templates.default = 'plotly_white'
 app = dash.Dash(__name__,
                 external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
-app.layout = html.Div(children=[
-    dcc.Graph(id='kmer-profile-graph',
-              config=dict(toImageButtonOptions=dict(format='svg'))),
-    html.Div(["DB file name: ",
-              dcc.Input(id='db-fname',
-                        value='',
-                        type='text')]),
-    html.Br(),
-    html.Div(["Read ID: ",
-              dcc.Input(id='read-id',
-                        value='1',
-                        type='number')]),
-    html.Br(),
-    html.Button(id='submit-button-state',
-                n_clicks=0,
-                children='Draw k-mer profile'),
-])
+
+
+### ----------------------------------------------------------------------- ###
+###                        k-mer count distribution                         ###
+### ----------------------------------------------------------------------- ###
+
+
+def load_count_dist(db_fname: str,
+                    max_count: int) -> Counter:
+    """Load global k-mer count frequencies in the database."""
+    if not os.path.exists(db_fname):
+        return Counter()
+    count_freqs = Counter()
+    command = f"REPkmer -x{max_count} {db_fname}"
+    lines = run_command(command).strip().split('\n')
+    for i, line in enumerate(lines):
+        if line.strip() == "K-mer Histogram":
+            lines = lines[i:]
+            break
+    for line in lines:
+        data = line.strip().split()
+        if len(data) != 3:
+            continue
+        kmer_count, freq, percent = data
+        kmer_count = kmer_count[:-1]
+        if kmer_count[-1] == '+':
+            kmer_count = kmer_count[:-1]
+        count_freqs[int(kmer_count)] = int(freq)
+    return count_freqs
 
 
 @app.callback(
-    Output('kmer-profile-graph', 'figure'),
-    [Input('submit-button-state', 'n_clicks')],
+    Output('kmer-count-dist', 'figure'),
+    [Input('submit-count-freq', 'n_clicks')],
     [State('db-fname', 'value'),
-     State('read-id', 'value')]
+     State('max-count', 'value')]
 )
-def update_kmer_profile(n_clicks: int,
-                        db_fname: str,
-                        read_id: str) -> go.Figure:
-    read_id = int(read_id)
-    counts = load_kmer_profile(db_fname, read_id)
-    return go.Figure(data=pl.make_scatter(x=list(range(len(counts))),
-                                          y=counts,
-                                          mode="lines",
-                                          col="black"),
-                     layout=pl.make_layout(x_title="Position",
-                                           y_title="Count",
-                                           x_grid=False,
-                                           y_grid=False))
+def update_count_dist(n_clicks: int,
+                      db_fname: str,
+                      max_count: int) -> go.Figure:
+    """Update the count distribution."""
+    if n_clicks == 0:
+        return go.Figure()
+    max_count = int(max_count)
+    count_freqs = load_count_dist(db_fname, max_count)
+    return go.Figure(data=pl.make_hist(count_freqs,
+                                       bin_size=1),
+                     layout=pl.make_layout(x_title="K-mer count",
+                                           y_title="Frequency"))
+
+
+### ----------------------------------------------------------------------- ###
+###                           k-mer count profile                           ###
+### ----------------------------------------------------------------------- ###
 
 
 def load_kmer_profile(db_fname: str,
@@ -68,5 +87,86 @@ def load_kmer_profile(db_fname: str,
     return counts
 
 
+@app.callback(
+    Output('kmer-profile-graph', 'figure'),
+    [Input('submit-kmer-profile', 'n_clicks')],
+    [State('db-fname', 'value'),
+     State('read-id', 'value')]
+)
+def update_kmer_profile(n_clicks: int,
+                        db_fname: str,
+                        read_id: str) -> go.Figure:
+    """Update the count profile plot."""
+    if n_clicks == 0:
+        return go.Figure()
+    read_id = int(read_id)
+    counts = load_kmer_profile(db_fname, read_id)
+    return go.Figure(data=pl.make_scatter(x=list(range(len(counts))),
+                                          y=counts,
+                                          mode="lines",
+                                          col="black"),
+                     layout=pl.make_layout(x_title="Position",
+                                           y_title="Count",
+                                           x_grid=False,
+                                           y_grid=False))
+
+
+### ----------------------------------------------------------------------- ###
+###                           Layout, Options, etc.                         ###
+### ----------------------------------------------------------------------- ###
+
+
+def main():
+    args = parse_args()
+    app.layout = html.Div(children=[
+        html.Div(["DB file: ",
+                  dcc.Input(id='db-fname',
+                            value=args.input_db,
+                            type='text')]),
+        html.Div(["Max count [for k-mer count distribution]: ",
+                  dcc.Input(id='max-count',
+                            value='1000',
+                            type='number')]),
+        html.Button(id='submit-count-freq',
+                    n_clicks=0,
+                    children='Draw k-mer count distribution'),
+        dcc.Graph(id='kmer-count-dist',
+                  config=dict(toImageButtonOptions=dict(format=args.download_as))),
+        html.Div(["Read ID: ",
+                  dcc.Input(id='read-id',
+                            value='',
+                            type='number')]),
+        html.Button(id='submit-kmer-profile',
+                    n_clicks=0,
+                    children='Draw k-mer profile'),
+        dcc.Graph(id='kmer-profile-graph',
+                  config=dict(toImageButtonOptions=dict(format=args.download_as)))
+    ])
+    app.run_server(debug=args.debug_mode)
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Visualizations for k-mer analysis")
+    parser.add_argument(
+        "-i",
+        "--input_db",
+        type=str,
+        help="Input DAZZ_DB file name.")
+    parser.add_argument(
+        "-f",
+        "--download_as",
+        type=str,
+        default="svg",
+        help="File format of the image downloaed via the icon.")
+    parser.add_argument(
+        "-d",
+        "--debug_mode",
+        action="store_true",
+        help="Run a Dash server in a debug mode.")
+    args = parser.parse_args()
+    return args
+
+
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    main()
